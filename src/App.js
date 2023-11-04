0cf933c94285f02a1ca0eb8c1fdd8938410fa1ef
Harisafzal03 <harisafzal503@gmail.com>
2023-11-04 21:53:59 +0500

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -14,21 +14,21 @@ function App() {
     setShowPassword(!showPassword);
   };
   return (
-    <div className='flex justify-between container mx-auto'>
-    <div className='mt-10 ml-20 mr-10' >
-     <img src={logo} alt="Logo" className="w-193 mb-10"  />
+    <div className='flex justify-between container mx-auto grid md:grid-rows-1 lg:flex md:m-0 sm:content-around '>
+    <div className='xl:mt-10 xl:ml-20 xl:mr-10 md:mt-10 md:ml-20 md:mr-10 lg:ml-10 lg:mt-24   ' >
+     <img src={logo} alt="Logo" className="w-193 mb-10 "  />
    
      <text className='font-family-Poppins mb-4 text-3xl font-semibold' alt="first">Get Started Now</text>
      <p></p>
      <text className='font-family-Poppins my-4 font-normal text-sm' alt="second">Enter your credentials to access your account</text>
-     <div className='flex gap-5 my-4'>
-      <button className='border border-gray-300 rounded-2xl w-60 h-[47px] flex justify-center items-center'>
+     <div className='flex justify-between my-4 sm:gap-3  sm:flex xs:grid xs:gap-y-2 '>
+      <button className='border border-gray-300 rounded-2xl xl:w-60 sm:w-40 md:w-60 lg:w-52 h-[47px] flex justify-center items-center'>
         <img src={google} alt="Google" />
-        <text className='text-sm font-medium pl-2'>Login with Google</text>
+        <text className='sm:text-sm font-medium pl-2 xs:text-xs'>Login with Google</text>
         </button>
-        <button className='border border-gray-300 rounded-2xl w-60 h-[47px] flex justify-center items-center'>
+        <button className='border border-gray-300 rounded-2xl xl:w-60 sm:w-40 md:w-60 lg:w-52 h-[47px] flex justify-center items-center'>
         <img src={Apple} alt="Apple" />
-        <text className='text-sm font-medium pl-2'>Login with Apple</text>
+        <text className='text-sm font-medium pl-2 xs:text-xs'>Login with Apple</text>
         </button>
 
      </div>
@@ -67,22 +67,22 @@ function App() {
     <div className='flex'>
     <input
     type="checkbox"
-    className="h-5 w-5 my-3 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+    className="h-5 w-5 my-2  text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
   />
   <label htmlFor="checkbox" className="ml-2 block text-gray-800 my-2 font-semibold text-sm">
   I agree to the
   </label> 
-  <label htmlFor="checkbox" className="ml-2 block text-gray-800 my-2 font-semibold text-sm underline">
+  <button className="ml-2 block text-gray-800 my-2 font-semibold text-sm underline">
   Terms & Privacy
-  </label> 
+  </button> 
   </div>  
-  <button className='bg-blue-900 w-full rounded-2xl h-[47px] my-2'>login</button> 
+  <button className='bg-blue-900 text-white w-full rounded-2xl h-[47px] my-2'>login</button> 
 <text className='my-4 font-semibold text-sm'>Don’t have an account?</text>
 <button className='my-4 font-semibold text-sm text-blue-400 underline pl-2' onClick={()=>{alert("hey")}}> Sign up</button>
     </div>
 
-    <div className='flex justify-center items-center'>
-    <img src={right} alt="right" className="h-[90%]"  />
+    <div className='flex justify-center items-center  '>
+    <img src={right} alt="right" className="xl:h-[90%] md:h-[90%]   lg:h-full "  />
     </div>
     </div>
     
